9237882c2d377730638727a0c611d55bf6847eee
Lukas427 <lukas.holleis05@htl-salzburg.ac.at>
2023-05-31 17:50:47 +0200

    more work on manager

--- Assets/MyGame/Scripts/GameManager.cs
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    public GameState State;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     public int gameState;
+
+    public void UpdateGameState(GameState newState)
+    {
+        State = newState;
+
+        switch (newState)
+        {
+            case GameState.FirstScreen:
+                break;
+            case GameState.SecondScreen:
+                break;
+            case GameState.ThirdScreen:
+            default:
+                break;
+        }
+    }
+
+    public void BtnPressed(int btnNumber)
+    {
+        if (btnNumber == 0)
+        {
+
+        }
+
+        if (btnNumber == 1)
+        {
+
+        }
+    }
+
+    public enum GameState
+    {
+        FirstScreen,
+        SecondScreen,
+        ThirdScreen
+    }
 }
