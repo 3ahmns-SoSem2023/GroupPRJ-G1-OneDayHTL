79dc5036b5ed629ce0b748ba7bab6812024aabca
JansenGrizzly <janesangerer05@gmail.com>
2023-06-22 15:14:28 +0200

    Text ist fertig und eingefügt in alle script dialog felder

--- Assets/MyGame/Scripts/TxT2.cs
+++ Assets/MyGame/Scripts/TxT2.cs
@@ -7,28 +7,31 @@ public class TxT2 : MonoBehaviour
     public GameManager2 gameManager;
     public void TextStart()
     {
-        gameManager.texts[0].SetText("First Scene");
+        gameManager.texts[0].SetText("Du Bist an der Schule angekommen! Gratuliere!");
         gameManager.texts[1].SetText("In School");
         gameManager.texts[2].SetText(gameManager.efctStorage.FetchStatus());
-        gameManager.texts[3].SetText("Choose");
-        gameManager.texts[4].SetText("SlackOff");
-        gameManager.texts[5].SetText("WorkHard");
+        gameManager.texts[3].SetText("Was als nächstes?");
+        gameManager.texts[4].SetText("Erstmal bisschen Chillen. Kollegas sind in der Kafetaria.");
+        gameManager.texts[5].SetText("Okay. Ich geh gleich in die Klasse und gib alles heute.");
     }
 
 
     public void TextEnding2()
     {
-        gameManager.texts[7].SetText("Ending2");
+        gameManager.texts[7].SetText("Du hast den Überraschungstest komplett verpasst, weil du die Zeit vergessen hast, während du mit deine Kollegas in der Cafeteria gegessen hast.");
     }
 
     public void TextEnding3()
     {
-        gameManager.texts[7].SetText("Ending3");
+        gameManager.texts[7].SetText("Du hast den ganzen Tag hart gearbeitet und den Überraschungstest mit Leichtigkeit bestanden.");
     }
+
+    
+    // für das ending 11 fehlt die "tired" bzw "energy?" zwischenabfrage für einen Energ
 
     public void TextEnding11()
     {
-        gameManager.texts[7].SetText("Ending11");
+        gameManager.texts[7].SetText("du bist ohne grund mit 5%iger wahrscheinlichkeit gestorben lol. HTL eben");
     }
 
 }
